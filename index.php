26e2c13ac96d6d8673ca41c9501a01f304c93bc8
BANDI SUNIL REDDY <90695256+SunilReddy54@users.noreply.github.com>
2021-09-14 21:47:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Bandi Sunil Reddy PHP</title>
+</head>
+<body>
+<h1>Bandi Sunil Reddy PHP</h1>
+<p>The SHA256 hash of "Bandi Sunil Reddy" is
+<?php
+print hash('sha256', 'Bandi Sunil Reddy');
+?>
+</p>
+<pre>ASCII ART:
+
+    ************
+    *          *
+    *          *
+    ************
+    *          *
+    *          *
+    ************
+
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
+</html>
